efd97b2827de5e3e669485ed2966d8f36796f92c
GovinduKamburugamuwa <114232870+GovinduKamburugamuwa@users.noreply.github.com>
2022-12-07 23:47:13 +0530

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function opentab(tabname)
+{
+ 
+   var tablinks = document.getElementsByClassName("tab-links");
+   var tabcontents = document.getElementsByClassName("tab-contents");
+   for(tablink of tablinks) 
+   {
+    tablink.classList.remove("active-links");
+   }
+   for(tabcontent of tabcontents) 
+   {
+    tabcontent.classList.remove("active-tab");
+   }
+   event.currentTarget.classList.add("active-links");
+   document.getElementById(tabname).classList.add("active-tab");
+
+}
